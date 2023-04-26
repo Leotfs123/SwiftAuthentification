f219396d3685e33e40f550d6d70d0519a49dce80
Leotfs123 <lechen@tfs.ca>
2023-04-26 14:57:49 -0400

    Initial Commit

---
+++ SwiftAuthentification/SwiftAuthentificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAuthentificationApp.swift
+//  SwiftAuthentification
+//
+//  Created by Leo Chen on 2023-04-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAuthentificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
